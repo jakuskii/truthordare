c0f5a3149b98f72facf0516dde459a7d7a46aad1
jakuskii <74100491+jakuskii@users.noreply.github.com>
2021-06-23 17:13:08 +0200

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('hamoud').addEventListener("click", function() {
-	document.querySelector('.code').style.display = "flex";
-});
-
-document.querySelector('.close').addEventListener("click", function() {
-	document.querySelector('.code').style.display = "none";
-});
