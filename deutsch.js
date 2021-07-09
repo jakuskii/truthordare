d53422515df73b78dc5b54a3d624651260592b77
jakuskii <74100491+jakuskii@users.noreply.github.com>
2021-07-09 21:01:41 +0200

    Update deutsch.js

--- deutsch.js
+++ deutsch.js
@@ -90,7 +90,7 @@ function addPlayers() {
 	// openPopup returns a destructor function
 	let closePopup = openPopup();
 	let popup = document.querySelector(".popup");
-	popup.appendChild(createEl("h2", "Spieler hinzufügen min. 2");
+	popup.appendChild(createEl("h2", "Spieler hinzufügen min. 2"));
 
 	let plrList = document.createElement("ul", HTMLUListElement);
 	plrList.className = "playerList";
